869fa91ab8fc723bade9ac9b7d4e6945ba689f1f
veselin.petrov <veselin.petrov.ext@racingpost.com>
2022-12-13 02:38:48 +0200

    y2022 day 13 - start, wip

---
+++ src/main/java/y2022/day13/Day13.java
@@ -0,0 +1,76 @@
+package y2022.day13;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import static helper.InputLoader.closeInput;
+import static helper.InputLoader.getMainIn;
+import static helper.InputLoader.loadInput;
+
+public class Day13 {
+
+    private static final Logger logger = LoggerFactory.getLogger(Day13.class.getSimpleName());
+    private static final String INPUT_FILE_NAME = "year_2022/day13_input.txt";
+//    private static final String INPUT_FILE_NAME = "debug.txt";
+
+    static List<String> inputLines = new ArrayList<>();
+    private static String solution;
+
+
+    public static void main(String[] args) {
+        logger.info("----   ADVENT Of code   2022    ----");
+        long start = new Date().getTime();
+        logger.info(":::START = " + LocalDateTime.ofEpochSecond(start / 1000, 0, ZoneOffset.ofHours(2)));
+        logger.info("                ---=== Day 13 ===---     ");
+        logger.info("                  - XXXXXXXXX -     ");
+
+        logger.info("    ---=== Part 1 ===---     ");
+
+        loadInput(INPUT_FILE_NAME, "");
+        while (getMainIn().hasNextLine()) {
+            String line = getMainIn().nextLine();
+            inputLines.add(line);
+        }
+
+        solvePartOne();
+
+        long p2Start = new Date().getTime();
+        logger.info("P1 Duration: " + (p2Start - start) + "ms (" + (p2Start - start) / 1000 + "s)");
+
+        logger.info("=========================================================================================");
+        logger.info("    ---=== Part 2 ===---     ");
+
+        solvePartTwo();
+
+        closeInput();
+
+
+        long end = new Date().getTime();
+        logger.info("P2 Duration: " + (end - p2Start) + "ms (" + (end - p2Start) / 1000 + "s)");
+        logger.info("==========");
+        logger.info("Total Duration: " + (end - start) + "ms (" + (end - start) / 1000 + "s)");
+
+        logger.info(":::END = " + end);
+        logger.info(":::END = " + LocalDateTime.ofEpochSecond(end / 1000, 0, ZoneOffset.ofHours(2)));
+    }
+
+    private static void solvePartOne() {
+
+
+        logger.info("    Part 1 solution:\n XXXXXX= [{}]", "<solution_goes_here>");
+
+    }
+
+    private static void solvePartTwo() {
+
+
+        logger.info("    Part 2 solution:\n YYYYYYYYYYYY= [{}]", "<solution_goes_here>");
+    }
+
+}
