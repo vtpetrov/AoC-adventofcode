eddca58f996f8ea74cd1ebd6f4cc10a47a33a7b0
Veselin Petrov <veselin.t.petrov@gmail.com>
2023-12-06 04:05:55 +0200

    day4 - part 2 wip....

--- src/main/java/y2023/day4/Scratchcards.java
+++ src/main/java/y2023/day4/Scratchcards.java
@@ -17,9 +17,11 @@ import static java.util.Arrays.stream;
 @Slf4j
 public class Scratchcards extends BaseDay {
 
+    private static boolean isCalcMatchesInvoked = false;
+
     static {
-//        inputFileName = "year_2023/day04_input.txt";
-        inputFileName = "debug.txt";
+        inputFileName = "year_2023/day04_input.txt";
+//        inputFileName = "debug.txt";
     }
 
     static String year = "2023";
@@ -29,10 +31,13 @@ public class Scratchcards extends BaseDay {
     static List<List<Integer>> cardsWinningNumbers = new ArrayList<>();
     static List<List<Integer>> cardsMyNumbers = new ArrayList<>();
     static List<Integer> cardMatches = new ArrayList<>();
+    static List<Integer> cardCoppies = new ArrayList<>();
     static List<Long> cardPoints = new ArrayList<>();
     private static Long sumTotal = 0L;
 
     static List<Integer> cardInstances = new ArrayList<>();
+    static List<List<Integer>> spawningCards = new ArrayList<>();
+
 
     public static void main(String[] args) {
         logStartP1(year, day, puzzleTitle);
@@ -42,7 +47,9 @@ public class Scratchcards extends BaseDay {
 
         solvePartOne();
         logStartP2();
+
         solvePartTwo();
+
         closeInput();
         logEndP2();
     }
@@ -66,7 +73,7 @@ public class Scratchcards extends BaseDay {
     }
 
     private static void solvePartOne() {
-        // per card for each number of mine matching a winning number, calculate the prize => 2^n-1 (n - the number of matches)
+
         calcMatches();
 
         solutionP1 = sumTotal;
@@ -78,6 +85,7 @@ public class Scratchcards extends BaseDay {
     }
 
     private static void calcMatches() {
+        isCalcMatchesInvoked = true;
         for (int i = 0; i < inputLines.size(); i++) {
             cardsMyNumbers.get(i).retainAll(cardsWinningNumbers.get(i));
             cardMatches.add(cardsMyNumbers.get(i).size());
@@ -90,22 +98,65 @@ public class Scratchcards extends BaseDay {
 
     private static void solvePartTwo() {
 
-        for (int i = 0; i < cardMatches.size(); i++) {
-            // find spawned instances based on matches:
-            Integer spawnNextXCards = cardMatches.get(i);
+        if (!isCalcMatchesInvoked) {
+            calcMatches();
         }
+
+
+        int currentCardIdx = 0;
+        cardCoppies.add(0); // set the number of copies for Card 1 to 0 (because it starts the sequence and doesn't have copies)
+        while (currentCardIdx < cardMatches.size() - 1) {
+            int currCardMatches = cardMatches.get(currentCardIdx);
+
+            // spawn copies:
+            int from = currentCardIdx + 1;
+            int to = from + currCardMatches;
+            // process ORIGINALS:
+            for (int j = from; j < to; j++) {
+                try {
+                    Integer old = cardCoppies.get(j);
+                    cardCoppies.set(j, ++old);
+                } catch (IndexOutOfBoundsException e) {
+                    cardCoppies.add(1);
+                }
+            }
+
+            // get the number of copies of the ORIGINAL card currently being processed
+            Integer currCardCopies = cardCoppies.get(currentCardIdx);
+            log.info("currCardCopies= {}", currCardCopies);
+            for (int c = from; c < to; c++) {
+                // increment FROM-TO in COPIES by adding 'currCardCopies' to them
+                Integer newVal;
+                try {
+                    newVal = cardCoppies.get(c) + currCardCopies;
+                    cardCoppies.set(c, newVal);
+                } catch (IndexOutOfBoundsException e) {
+                    cardCoppies.add(currCardCopies);
+                }
+            }
+
+
+//                // process COPIES:
+//                Integer currCardCopies = cardCoppies.get(currentCardIdx);// get the number of copies of the ORIGINAL card currently being processed
+//                Integer newVal = cardCoppies.get(j) + currCardCopies;
+//                cardCoppies.set(j, newVal);
+
+//            // account for a lot of 0 matches, sync copy list with 0 copies; copy list should follow the 'currentCardIdx'
+//            if(currentCardIdx >= cardCoppies.size()){
+//                for (int i = 0; i < currentCardIdx - cardCoppies.size() + 1; i++) {
+//                    cardCoppies.add(0);
+//                }
+//            }
+
+            currentCardIdx++;
+        }
+
+        solutionP2 = cardMatches.size() + cardCoppies.stream().mapToInt(Integer::intValue).sum();
 
         log.info("""
                 Part 2 solution:
-                 XXXXXX= [{}]""", solutionP2);
+                 Process all of the original and copied scratchcards until no more scratchcards are won.
+                 Including the original set of scratchcards, how many total scratchcards do you end up with?
+                 = [{}] ({})""", solutionP2, prettyPrintNumber((Number) solutionP2, '\''));
     }
-//    Card 1, has [4] matches   => 1
-//    Card 2, has [2] matches  c1  Card 2, has [2] matches  => 2
-//    Card 3, has [2] matches  c1  Card 3, has [2] matches   c2  Card 3, has [2] matches  cc2  Card 3, has [2] matches   => 4
-//    Card 4, has [1] matches  c1  Card 4, has [1] matches   c2  Card 4, has [1] matches  cc2  Card 4, has [1] matches   c3 Card 4, has [1] matches  c1  Card 4, has [1] matches   c2  Card 4, has [1] matches  cc2  Card 4, has [1] matches   => 8
-//    Card 5, has [0] matches  c1  Card 5, has [0] matches                                                               c3 Card 5, has [0] matches  c1  Card 5, has [0] matches
-//    Card 6, has [0] matches
-
-
-
 }
